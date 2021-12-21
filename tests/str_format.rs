48072d6a2b12e943e27fd713d200150443f4f6c6
洪伟鹏 <hongweipeng@bonree.com>
2021-12-21 17:10:17 +0800

    str format

---
+++ tests/str_format.rs
@@ -0,0 +1,101 @@
+use std::fmt::{self, Formatter, Display};
+
+#[test]
+fn test_str_format() {
+    // 通常情况下，`{}` 会被任意变量内容所替换。
+    // 变量内容会转化成字符串。
+    println!("{} days", 31);
+
+    // 不加后缀的话，31 就自动成为 i32 类型。
+    // 你可以添加后缀来改变 31 的类型（例如使用 31i64 声明 31 为 i64 类型）。
+
+    // 用变量替换字符串有多种写法。
+    // 比如可以使用位置参数。
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // 可以使用命名参数。
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // 可以在 `:` 后面指定特殊的格式。
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+
+    // 你可以按指定宽度来右对齐文本。
+    // 下面语句输出 "     1"，5 个空格后面连着 1。
+    println!("{number:>width$}", number=1, width=6);
+
+    // 你可以在数字左边补 0。下面语句输出 "000001"。
+    println!("{number:>0width$}", number=1, width=6);
+
+    // println! 会检查使用到的参数数量是否正确。
+    // println!("My name is {0}, {1} {0}", "Bond");
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // 改正 ^ 补上漏掉的参数："James"
+
+    // 创建一个包含单个 `i32` 的结构体（structure）。命名为 `Structure`。
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    // println!("This struct `{}` won't print...", Structure(3));
+    // 改正 ^ 注释掉此行。
+}
+
+
+#[test]
+fn test_struct_debug_format() {
+    // 为结构添加 #[derive(Debug)] 属性，使其能有默认输出
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    // 打印
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{:?}", *color)
+    }
+}
+
+
+#[test]
+fn test_struct_custom_format() {
+    struct City {
+        name: &'static str,
+        // 纬度
+        lat: f32,
+        // 经度
+        lon: f32,
+    }
+
+    impl Display for City { // 自定义结构体的输出
+        // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+            // 一个缓冲区（即第一个参数f）中。
+            write!(f, "{}: {:.3}°{} {:.3}°{}",
+                   self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+        }
+    }
+
+    // 打印
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+}
